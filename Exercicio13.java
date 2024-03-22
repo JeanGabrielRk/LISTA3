63eba8af53d41b6c95d7f14444153cf58b03cd79
Jean <jeangabriel058@gmail.com>
2024-03-22 16:25:11 -0300

    Exercicio13

---
+++ Exercicio13.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Exercicio13 {
+
+    public static void executar() {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Informe quantas inserções: ");
+        int n = leitor.nextInt();
+
+        String[] nome = new String[n];
+        String[] sexo = new String[n];
+        int[] idade = new int[n];
+        String[] saude = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Digite o nome: ");
+            nome[i] = leitor.next();
+
+            System.out.println("Digite o sexo: ");
+            sexo[i] = leitor.next();
+
+            System.out.println("Digite a idade: ");
+            idade[i] = leitor.nextInt();
+
+            System.out.println("Digite a saúde (Apto/Inapto): ");
+            saude[i] = leitor.next();
+
+            if (Verificacao.estaAptoServicoMilitar(sexo[i], idade[i], saude[i])) {
+                System.out.println(nome[i] + " está apto para cumprir o serviço militar obrigatório.");
+            } else {
+                System.out.println(nome[i] + " não está apto para cumprir o serviço militar obrigatório.");
+            }
+        }
+
+        leitor.close();
+    }
+}
